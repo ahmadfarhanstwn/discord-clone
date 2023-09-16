ea49b528234aa6e021d484096bcb370fee6f66a1
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2023-09-16 21:26:19 +0700

    dark and light theme

--- app/(main)/(route)/page.tsx
+++ app/(main)/(route)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "../../../components/ui/button";
+import { ModeToggle } from "../../../components/ModeToggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
