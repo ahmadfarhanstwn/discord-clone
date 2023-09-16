d33865d0ac52021e91683fa91a100941c61160ae
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2023-09-16 20:17:33 +0700

    auth

--- app/(main)/(route)/page.tsx
+++ app/(main)/(route)/page.tsx
@@ -1,11 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "../../../components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-bold text-indigo-500'>
-        Protected Route
-      </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
