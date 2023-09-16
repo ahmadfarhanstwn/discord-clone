3c0dc23500c7f00c3a7557e0e527d10fb01af5be
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2023-09-16 19:37:28 +0700

    folder setup

--- app/(main)/(route)/page.tsx
+++ app/(main)/(route)/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "../components/ui/button";
+import { Button } from "../../../components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <p className='text-3xl font-bold text-indigo-500'>
-        Discord Clone
+        Protected Route
       </p>
-      <Button variant={'destructive'}>Click</Button>
     </div>
   )
 }
