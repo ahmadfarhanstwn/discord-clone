92aa5a124909619d8450c215afa3ac8e18c7f314
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2023-09-24 16:21:39 +0700

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,41 @@
-const ChannelIdPage = () => {
+import React from "react";
+import { CurrentProfile } from "../../../../../../../lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "../../../../../../../lib/db";
+import { redirect } from "next/navigation";
+import ChatHeader from "../../../../../../../components/chats/ChatHeader";
+
+interface ChannelIdPageProps {
+    params : {
+        serverId: string,
+        channelId: string
+    }
+}
+
+const ChannelIdPage: React.FC<ChannelIdPageProps> = async ({params}) => {
+    const profile = await CurrentProfile()
+
+    if(!profile) return redirectToSignIn()
+
+    const channel = await db.channel.findFirst({
+        where: {
+            id: params.channelId
+        }
+    })
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id
+        }
+    })
+
+    if (!channel || !member) return redirect("/")
+    
     return ( 
-        <div></div>
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+        </div>
      );
 }
  
